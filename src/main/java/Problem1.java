6253b7347cde625eccd6d95a75980f45d2c0dce8
IacovellaWCU <89997414+IacovellaWCU@users.noreply.github.com>
2024-09-27 19:53:23 -0400

    Update Problem1.java

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -4,10 +4,20 @@ public class Problem1 {
         // TODO: Implement the concatenate method
         // This method should concatenate l2 to l1 and return l1
         // Remember to create new nodes instead of directly linking l1 to l2
-        ListNode out = createList();
-        out.addAll(l1);
-        out.addAll(l2);
+        // Problem1.java
+        ListNode poinby = null;
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        poinby = l1;
+        while (poinby.next != null) {
+            poinby = poinby.next;
+        } 
+        poinby.next = l2;
         
-        return out;
+        return l1;
     }
 }
