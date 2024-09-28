0cbdff7ab0ac157fe4350ccff4a7fa1ee87666ce
IacovellaWCU <89997414+IacovellaWCU@users.noreply.github.com>
2024-09-27 21:58:50 -0400

    Update Problem2.java

--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -4,7 +4,7 @@ public class Problem2 {
       ListNode l = null;
       ListNode l_iterator = null;
 
-        if (position = 0) {
+        if (position == 0) {
             l = new ListNode(l1.val);
         } else {
             l = new ListNode(l1.val);
