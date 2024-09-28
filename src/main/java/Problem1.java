14810301aa0d33a91a762b93cac340b4f892349d
IacovellaWCU <89997414+IacovellaWCU@users.noreply.github.com>
2024-09-27 21:09:37 -0400

    Update Problem1.java

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -5,15 +5,17 @@ public class Problem1 {
         // This method should concatenate l2 to l1 and return l1
         // Remember to create new nodes instead of directly linking l1 to l2
         // Problem1.java
-        ListNode l3 = ListNode(-1);
-        if (l1 == null) {
-          return l2;
-        }
-        if (l2 == null) {
-          return l1;
-        }
-        int curr = l3.val;
+        
+        // if (l1 == null) {
+        //   return l2;
+        // }
+        // if (l2 == null) {
+        //   return l1;
+        // }
+        // ListNode l3 = l1;
+        // int curr = l3.val;
         while (l1.next != null) {
+            print(l1);
             curr = l1.val;
             curr = l3.next.val;
         }
