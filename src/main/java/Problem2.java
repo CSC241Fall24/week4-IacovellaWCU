41c8cb39b97d9586ef61ee97c7c3a73bcac01b4f
IacovellaWCU <89997414+IacovellaWCU@users.noreply.github.com>
2024-09-27 22:46:00 -0400

    Update Problem2.java

--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -2,43 +2,24 @@
 public class Problem2 {
     public static ListNode insert(ListNode head, int val, int position) {
       ListNode l = null;
-      ListNode l_iterator = null;
 
-        ListNode l1_iterator = head.next;
-        l_iterator = new ListNode(l1_iterator.val);
-        if (position == 1) {
-             l = new ListNode(val);
-             l.next = new ListNode(head.val);
-            l_iterator = l.next;
+
+        if (position == 0) {
+            ListNode start = val;
         } else {
-            l = new ListNode(head.val);
-            l.next = l_iterator;
+            ListNode start = head;
         }
-        
-       
-        int i = 1;
+
+        ListNode l = start;
+        for (int i = 1; i < position; i++) {
+            l.next = head.next;
+        }
+        if (head.next != null) {
+            start.next = head.next;
+        }
 
         
-        
-        while (i <= position && l1_iterator.next != null) {
-            l_iterator.next = new ListNode(l1_iterator.next.val);
-            l_iterator = l_iterator.next;
-            l1_iterator = l1_iterator.next;
-            i = i + 1;
-        }
-        if (position != 1) {
-            l_iterator.next = new ListNode(val);
-            l_iterator = l_iterator.next;
-            l1_iterator = l1_iterator.next;
-        }
-        
-        while (l1_iterator != null && l1_iterator.next != null) {
-            l_iterator.next = new ListNode(l1_iterator.next.val);
-            l_iterator = l_iterator.next;
-            l1_iterator = l1_iterator.next;
-        }
-      
-
+    
       return l;
     }
 }
