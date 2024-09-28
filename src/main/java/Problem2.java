313931ca677e6649a88af2b2ae58908eccb3f7ec
IacovellaWCU <89997414+IacovellaWCU@users.noreply.github.com>
2024-09-27 22:51:29 -0400

    Update Problem2.java

--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -1,25 +1,18 @@
 
 public class Problem2 {
     public static ListNode insert(ListNode head, int val, int position) {
-      ListNode l = null;
-
-
-        if (position == 0) {
-            ListNode start = val;
-        } else {
-            ListNode start = head;
+        int i = 0;
+        ListNode new_head =  new ListNode(-1);
+        ListNode iterator = new_head;
+        iterator.next = head;
+        while(i < position - 1 && iterator.next != null) {
+            iterator = iterator.next;
+            i++;
         }
+        ListNode second_half = iterator.next;
+        iterator.next = new ListNode(val);
+        iterator.next.next = second_half;
+        return new_head.next;
 
-        ListNode l = start;
-        for (int i = 1; i < position; i++) {
-            l.next = head.next;
-        }
-        if (head.next != null) {
-            start.next = head.next;
-        }
-
-        
-    
-      return l;
     }
 }
