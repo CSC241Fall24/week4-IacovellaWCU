fff2f9a0fd71eeb8fe76b40961292e5ebe62521e
IacovellaWCU <89997414+IacovellaWCU@users.noreply.github.com>
2024-09-27 22:11:25 -0400

    Update Problem2.java

--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -6,9 +6,17 @@ public class Problem2 {
 
         ListNode l1_iterator = head.next;
         l_iterator = new ListNode(l1_iterator.val);
-        l = new ListNode(head.val);
+        if (position == 0) {
+             l = new ListNode(val);
+             l.next = new ListNode(head.val);
+        } else {
+            l = new ListNode(head.val);
+        }
+        
         l.next = l_iterator;
         int i = 0;
+
+        
         
         while (i < position && l1_iterator.next != null) {
             l_iterator.next = new ListNode(l1_iterator.next.val);
@@ -16,7 +24,10 @@ public class Problem2 {
             l1_iterator = l1_iterator.next;
             i = i + 1;
         }
-        l_iterator.next = new ListNode(val);
+        if (i != 0) {
+            l_iterator.next = new ListNode(val);
+        }
+        
         while (l1_iterator.next != null) {
             l_iterator.next = new ListNode(l1_iterator.next.val);
             l_iterator = l_iterator.next;
